7c7dbfab9ac6b0f76ba770b7b19c3a88f1eb9389
bretzle <johnfish218@gmail.com>
2025-03-30 21:01:08 -0500

    first pass at ttf

--- examples/simple.zig
+++ examples/simple.zig
@@ -2,12 +2,16 @@ const std = @import("std");
 const prism = @import("prism");
 const math = prism.math;
 const gpu = prism.gpu;
+const ttf = prism.file.ttf;
+
+const font_data = @embedFile("roboto.ttf");
 
 const App = prism.Application(struct {
     const Self = @This();
 
     batch: gpu.Batch,
     tex: gpu.TextureId,
+    text: gpu.TextureId,
 
     pub fn init(self: *Self) !void {
         self.batch = try .create(prism.allocator);
@@ -19,6 +23,37 @@ const App = prism.Application(struct {
 
         @memset(&buf, 0);
         gpu.updateTexturePart(self.tex, 1, 1, 23, 23, &buf);
+
+        const font = try ttf.Font.loadmem(font_data);
+        defer font.deinit();
+
+        const sft = ttf.SFT{
+            .font = font,
+            .x_scale = 32,
+            .y_scale = 32,
+            .flags = ttf.SFT_DOWNWARD_Y,
+        };
+
+        const glyph = try sft.lookup('@');
+        const mtx = try sft.gmetrics(glyph);
+
+        var img = ttf.Image{
+            .width = (mtx.min_width + 3) & ~@as(u32, 3),
+            .height = mtx.min_height,
+            .pixels = undefined,
+        };
+        img.pixels = try prism.allocator.alloc(u8, @intCast(img.width * img.height));
+        defer prism.allocator.free(img.pixels);
+
+        try sft.render(glyph, img);
+
+        self.text = gpu.createTexture(.{
+            .width = img.width,
+            .height = img.height,
+            .format = .r,
+            // .content = img.pixels.ptr,
+        });
+        gpu.updateTexture(self.text, img.pixels);
     }
 
     pub fn update(_: *Self, app: *App) void {
@@ -33,7 +68,8 @@ const App = prism.Application(struct {
 
         self.batch.pushMatrix(transform);
         self.batch.drawRect(.{ .x = -32, .y = -32, .w = 64, .h = 64 }, .red);
-        self.batch.drawTexture(self.tex, .{ .x = 64, .y = -32 });
+        // self.batch.drawTexture(self.tex, .{ .x = 64, .y = -32 });
+        self.batch.drawTexture(self.text, .{ .x = 100, .y = 0 });
         _ = self.batch.popMatrix();
 
         self.batch.render(gpu.framebufferSize());
--- src/file.zig
+++ src/file.zig
@@ -1,1 +1,2 @@
 pub const qoi = @import("file/qoi.zig");
+pub const ttf = @import("file/ttf.zig");
